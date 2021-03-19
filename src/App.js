1269a36b9ef1eb74b6dee6b25e5ba8ee15355263
gensbarahonab <gens_barahonab@unicah.edu>
2021-03-18 18:40:51 -0600

    primer comentario

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+//import Todo from './Contenido/Todo/Todo';
+import BrandHeader from './Contenido/BrandHeader/BrandHeader';
+
+
+function App() {
+  return (
+     <BrandHeader></BrandHeader>
+     
+    
+  );
+}
+
+export default App;
